839a34a53805de9af203e53931c5b6e2ed19d2cc
root <kiba.rain@qq.com>
2018-05-03 12:25:53 +0000

    完成route search功能

--- src/route.js
+++ src/route.js
@@ -1,10 +1,77 @@
-const Route = require('glob'),
-    path = require('path');
+const Glob = require('glob'),
+    Path = require('path'),
+    Is = require('./is');
 
-exports.koa = (root)=>{
-    
-    return (path,method)=>{
+function promisify(cb){
+    return function (...args) {
+        const self = this;
+        return new Promise((resolve,reject)=>{
+            args.push(
+                (e,r)=>e === undefined || e===null?resolve(r):reject(e)
+            );
+            cb.apply(self,args);
+        });
+    }
+}
 
+function toURI(path){
+    return slash === sep?path:path.replace(sep,slash);
+}
+
+const {sep,relative,basename,dirname} = Path,
+    {fn,object} = Is,
+    glob = promisify(Glob),
+    pattern = '**/*.js',
+    root = '',
+    dot = '.',
+    ext = '.js',
+    slash = '/',
+    index = 'index';
+
+function init(fixed,files){
+    const cache = {};
+    let n,d,u;
+    for(let file of files){
+        n = basename(file,ext);
+        d = dirname(file);
+        u = d == dot?root:toURI(d)
+        k = u === root?n:[u,n].join(slash);
+        cache[k] = [fixed,k].join(slash);
+        if(
+            n == index 
+            && !cache.hasOwnProperty(u)
+        ){
+            cache[u] = cache[k];
+        }
+    }
+    return cache;
+}
+
+function resolvePath(path){
+    return path[0] === slash?path.slice(1):path;
+}
+
+function resolveMethod(method){
+    return method.toLowerCase();
+}
+
+exports.search = (cwd)=>{
+
+    const fixed = toURI(relative(__dirname,cwd)),
+        mappings = glob(pattern,{cwd})
+            .then(_=>init(fixed,_));
+
+    return async (path,method) => {
+        const cache = await mappings;
+        let uri,module,action;
+        if(uri = cache[resolvePath(path)]){
+            module = require(uri);
+            action = resolveMethod(method);
+            if(object(module))
+                return module[action];
+            if(fn(module))
+                return module(action);
+        }
     };
-    
+
 };
--- test/route.test.js
+++ test/route.test.js
@@ -1,28 +1,32 @@
 const method = 'METHOD',
-modules = [
-    ['normal',{method}],
-    ['path/normal',{method}],
-    ['path/test/index',{method}],
-    ['non',{}],
-],
-ext = '.js',
-glob = jest.fn(
-    (p,o,cb) => cb(
-        null, 
-        modules.map(_=>_[0] + ext)
-    )
-);
+    error = new Error(),
+    errorFn = ()=>{throw error;}
+    modules = [
+        ['normal',{method}],
+        ['path/normal',{method}],
+        ['path/test/index',{method}],
+        ['non',{}],
+        ['error',errorFn],
+        ['error/method',()=>errorFn]
+    ],
+    ext = '.js',
+    glob = jest.fn(
+        (p,o,cb) => cb(
+            null, 
+            modules.map(_=>_[0] + ext)
+        )
+    );
 jest.doMock('glob',()=>glob);
 
-const Route = require('../src/route');
+const Route = require('../src/route'),
+    Is = require('../src/is');
 const {search} = Route,
+    {fn} = Is,
     current = '.',
     sep = '/',
-    root = 'root',
+    root = __dirname,
     pattern = '**/*.js',
-    opts = {cwd:root},
-    error = new Error(),
-    errorFn = ()=>{throw error;};
+    opts = {cwd:root};
 
 jest.dontMock('glob');
 
@@ -33,37 +37,106 @@ describe('route',()=>{
         beforeAll(()=>{
             for(let module of modules)
                 jest.doMock(
-                    (current,root,module[0]).join(sep),
-                    ()=>module[1]
+                    [current,module[0]].join(sep),
+                    fn(module[1])?module[1]:()=>module[1],
+                    {virtual: true}
                 );
         });
         
         afterAll(()=>{
             for(let module of modules)
                 jest.dontMock(
-                    (current,root,module[0]).join(sep)
+                    [current,module[0]].join(sep)
                 );
         });
 
         afterEach(() => {
-            glob.mockReset();
+            glob.mockClear();
         });
 
-        it('not found module',()=>{
+        it('not found module',async ()=>{
             const route = search(root);
-
-            const app = await route('/','get');
+            
+            const app = await route('/',method),
+                app1 = await route('/',method);
 
             expect(glob).toHaveBeenCalledTimes(1);
-            expect(glob).toHaveBeenCalledWith(pattern,opts);
+            expect(glob).toHaveBeenCalledWith(pattern,opts,expect.anything());
             expect(app).toBeUndefined();
+            expect(app1).toBeUndefined();
         });
 
-        it.skip('multi not found module',()=>{});
-        it.skip('error',()=>{});
-        it.skip('multi error',()=>{});
-        it.skip('success',()=>{});
-        it.skip('multi success',()=>{});
+        it('not found method',async ()=>{
+            const route = search(root);
+            
+            const app = await route(modules[3][0],method),
+                app1 = await route(modules[3][0],method);
+
+            expect(glob).toHaveBeenCalledTimes(1);
+            expect(glob).toHaveBeenCalledWith(pattern,opts,expect.anything());
+            expect(app).toBeUndefined();
+            expect(app1).toBeUndefined();
+        });
+
+        it('error module',async ()=>{
+            const route = search(root);
+            
+            let err;
+            try{
+                await route(modules[4][0],method);
+            }catch(e){
+                err = e;
+            }
+            expect(err).toBe(error);
+ 
+            try{
+                await route(modules[4][0],method);
+            }catch(e){
+                err = e;
+            }
+            
+            expect(err).toBe(error);
+            expect(glob).toHaveBeenCalledTimes(1);
+            expect(glob).toHaveBeenCalledWith(pattern,opts,expect.anything());
+        });
+
+        it('error method',async ()=>{
+            const route = search(root);
+            
+            let err;
+            try{
+                await route(modules[5][0],method);
+            }catch(e){
+                err = e;
+            }
+            expect(err).toBe(error);
+
+            try{
+                await route(modules[5][0],method);
+            }catch(e){
+                err = e;
+            }
+
+            expect(err).toBe(error);
+            expect(glob).toHaveBeenCalledTimes(1);
+            expect(glob).toHaveBeenCalledWith(pattern,opts,expect.anything());
+        });
+
+        it('success',async ()=>{
+            const route = search(root);
+            
+            const app = await route(modules[0][0],method),
+                app1 = await route(modules[1][0],method),
+                app2 = await route('path/test',method),
+                app3 = await route(modules[0][0],method);
+
+            expect(glob).toHaveBeenCalledTimes(1);
+            expect(glob).toHaveBeenCalledWith(pattern,opts,expect.anything());
+            expect(app).toBe(modules[0][1].method);
+            expect(app1).toBe(modules[1][1].method);
+            expect(app2).toBe(modules[2][1].method);
+            expect(app3).toBe(modules[0][1].method);
+        });
     });
 
 });
